29b37cc854738ba6936a7ff6a3138be4363bb38b
Vedant <birlavedant13@gmail.com>
2023-06-25 22:47:33 +0530

    final change

--- src/components/Layout/Loader.jsx
+++ src/components/Layout/Loader.jsx
@@ -1,19 +1,11 @@
 import React from "react";
-import Lottie from "react-lottie";
-import animationData from "../../assets/24151-ecommerce-animation.json";
+import LoaderImg from "../../assets/loader.gif"
 
 const Loader = () => {
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: animationData,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-    },
-  };
+ 
   return (
     <div className="w-full h-screen flex items-center justify-center">
-      <Lottie options={defaultOptions} width={300} height={300} />
+      <img src={LoaderImg} alt="" />
     </div>
   );
 };
