3eb9266de0239077a2c3584cb36bcc9bce62dbf5
Vedant <birlavedant13@gmail.com>
2023-06-25 22:56:42 +0530

    final change

--- src/components/TrackOrder.jsx
+++ src/components/TrackOrder.jsx
@@ -2,7 +2,6 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getAllUserOrders } from "../redux/actions/order";
-import ProcessingLoader from "./Layout/ProcessingLoader";
 
 const TrackOrder = () => {
   const { orders } = useSelector((state) => state.order);
@@ -17,12 +16,10 @@ const TrackOrder = () => {
 
   const data = orders && orders.find((item) => item._id === id);
 
-  console.log(data);
   return (
     <div className="w-full h-screen flex flex-col items-center justify-center">
       {data && data?.status === "Processing" && (
         <div className="">
-          <ProcessingLoader />
           <h1 className="font-Poppins text-2xl ">
             Your order is Processing!!{" "}
           </h1>
@@ -30,7 +27,6 @@ const TrackOrder = () => {
       )}
       {data && data?.status === "Transferred to delivery partner" && (
         <div className="">
-          <ProcessingLoader />
           <h1 className="font-Poppins text-2xl 800px:text-lg text-center ">
             Your order is Transferred to delivery partner!!{" "}
           </h1>
@@ -38,7 +34,6 @@ const TrackOrder = () => {
       )}
       {data && data?.status === "Shipping" && (
         <div className="">
-          <ProcessingLoader />
           <h1 className="font-Poppins text-2xl ">
             Your order is Shipped for delivery!!{" "}
           </h1>
@@ -46,7 +41,6 @@ const TrackOrder = () => {
       )}
       {data && data?.status === "Received" && (
         <div className="">
-          <ProcessingLoader />
           <h1 className="font-Poppins text-2xl ">
             Your order is Received for delivery!!{" "}
           </h1>
@@ -54,7 +48,6 @@ const TrackOrder = () => {
       )}
       {data && data?.status === "On the way" && (
         <div className="">
-          <ProcessingLoader />
           <h1 className="font-Poppins text-2xl ">
             Your order will be delivered today!!{" "}
           </h1>
@@ -62,13 +55,11 @@ const TrackOrder = () => {
       )}
       {data && data?.status === "Delivered" && (
         <div className="">
-          {/* <ProcessingLoader  /> */}
           <h1 className="font-Poppins text-2xl ">Your order is Delivered!! </h1>
         </div>
       )}
       {data && data?.status === "Processing refund" && (
         <div className="">
-          <ProcessingLoader />
           <h1 className="font-Poppins text-2xl ">
             Your refund is Processing!!{" "}
           </h1>
@@ -76,7 +67,6 @@ const TrackOrder = () => {
       )}
       {data && data?.status === "Refund Success" && (
         <div className="">
-          <ProcessingLoader />
           <h1 className="font-Poppins text-2xl ">
             Your refund is Processed Successfully!!{" "}
           </h1>
--- src/pages/OrderSuccessPage.jsx
+++ src/pages/OrderSuccessPage.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import Footer from "../components/Layout/Footer";
 import Header from "../components/Layout/Header";
-import Lottie from "react-lottie";
-import animationData from "../assets/animation/107043-success.json"
 
 
 const OrderSuccessPage = () => {
@@ -16,17 +14,9 @@ const OrderSuccessPage = () => {
 };
 
 const Success = () => {
-  const defaultOptions = {
-    loop: false,
-    autoplay: true,
-    animationData: animationData,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-    },
-  };
+  
   return (
     <div>
-      <Lottie options={defaultOptions} width={300} height={300} />
       <h5 className="text-center mb-14 text-[25px] text-[#000000a1]">
         Your order is successful 😍
       </h5>
